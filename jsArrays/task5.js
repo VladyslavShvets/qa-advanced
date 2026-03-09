0c54796920a5f0016c6fd10d44a7640fd9781b0b
Влад <vlad@MacBook-Air-Vlad.local>
2026-03-09 02:25:27 +0200

    Add HW 8.1 arrays in JavaScript tasks

---
+++ jsArrays/task5.js
@@ -0,0 +1,7 @@
+const firstArray = [1, 2, 3];
+const secondArray = [4, 5, 6];
+const mergedArray = [...firstArray, ...secondArray];
+
+console.log("Перший масив:", firstArray);
+console.log("Другий масив:", secondArray);
+console.log("Об'єднаний масив:", mergedArray);
